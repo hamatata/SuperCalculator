03ab2b59011fcb8dc252fdf462251ff574f54c7e
Vitalii Onufryk <v.onufryk@gmail.com>
2019-05-19 19:23:47 +0300

    Calculator class with few operations

---
+++ myFirstCalc/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myFirstCalc
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+}
--- myFirstCalc/Program.cs
+++ myFirstCalc/Program.cs
@@ -6,7 +6,19 @@ namespace myFirstCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator calc = new Calculator();           
+            Console.WriteLine("Yahoo, look at our first Calculator: " + calc);
+
+            int a = 5;
+            int b = 6;
+            int resSum = calc.add(a, b);
+            Console.WriteLine(a + " + " + b + " = " + resSum);
+
+            int resMmultiply = calc.multiply(a, b);
+            Console.WriteLine(a + " * " + b + " = " + resMmultiply);
+
+            int resSubstr = calc.minus(a, b);
+            Console.WriteLine(a + " - " + b + " = " + resSubstr);
         }
     }
 }
